963c1114ba11d8e1b8417f937372853d2a752a7e
Eray <129649763+Luciflerr@users.noreply.github.com>
2024-07-05 16:18:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,39 @@ let scor1btn = document.querySelector('#scor1');
 let scor2btn = document.querySelector('#scor2');
 let rst = document.querySelector('#res');
 
-scor1btn.addEventListener('click', function(e){
-    scores1.textContent = parseInt(scores1.textContent) +1;
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let gameOver = false;
+
+
+scor1btn.addEventListener('click', function(){
+    if (!gameOver){
+        p1Score += 1; //bu kısımın üstte olmasının sebebi 1 arttırdığımızda text contente ulaşıp orda da 1 arttırması.
+        if (p1Score === winningScore) {
+            gameOver = true;
+            scores1.style.color ='green';
+            scores2.style.color ='red';
+        }
+        scores1.textContent = p1Score;
+    }
 });
 
-scor2btn.addEventListener('click', function(e){
-    scores2.textContent = parseInt(scores2.textContent) +1;
+scor2btn.addEventListener('click', function(){
+    if(!gameOver){
+        p2Score += 1;
+        if(p2Score === winningScore) {
+            gameOver = true;
+            scores2.style.color ='green';
+            scores1.style.color ='red';
+        }
+        scores2.textContent = p2Score;
+    }
 });
 
-rst.addEventListener('click', function(e) {
+rst.addEventListener('click', function() {
     scores1.textContent = 0;
     scores2.textContent = 0;
+    p1Score = 0;
+    p2Score = 0;
 });
-
-
